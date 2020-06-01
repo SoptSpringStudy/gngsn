cc501778ad9f3e853096c8f780cf589dd131ff8e
gngsn <rudtjs4540@gmail.com>
2020-06-01 18:22:24 +0900

    Update week3

--- week3/DIPractice/src/main/java/com/spring/StudentMain.java
+++ week3/DIPractice/src/main/java/com/spring/StudentMain.java
@@ -3,6 +3,7 @@ package com.spring;
 import com.spring.dao.Student;
 import com.spring.services.StudentRegisterService;
 import com.spring.services.StudentSearchService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 
 import java.util.Scanner;
@@ -12,8 +13,10 @@ public class StudentMain {
         String[] name = {"mj", "gs", "dk", "hs", "hw"};
         int[] age = {24, 23, 26, 24, 24};
         String[] soptDept = {"server", "server", "server", "android", "android"};
-        String[] ctxs = {"classpath:appContext.xml"};
-        GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(ctxs);
+        // context.xml - Bean 객체를 생성해 줌
+        // String[] ctxs = {"classpath:appContext.xml"};
+//      GenericXmlApplicationContext ctx = new GenericXmlApplicationContext(ctx);
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(StudentConfig.class);
 
         StudentRegisterService registerService = ctx.getBean("studentRegisterService", StudentRegisterService.class);
         for (int i =0; i < name.length; i++) {
--- week3/DIPractice/src/main/java/com/spring/services/StudentRegisterService.java
+++ week3/DIPractice/src/main/java/com/spring/services/StudentRegisterService.java
@@ -1,9 +1,11 @@
 package com.spring.services;
 import com.spring.dao.Student;
 import com.spring.dao.StudentDao;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 
-public class StudentRegisterService {
+public class StudentRegisterService implements InitializingBean, DisposableBean {
     static private StudentDao studentDao;
 
     public StudentRegisterService(){}
@@ -14,5 +16,15 @@ public class StudentRegisterService {
     public static void register(Student student) {
         studentDao.insert(student);
         System.out.println("Main Dao 추가했습니다 ~");
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Bean 객체 생성");
+    }
+
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("Bean 객체 소멸");
     }
 }
